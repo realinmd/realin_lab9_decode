f4dc768995893a767fd28602a7773eaa0cb89657
Realin Damuth <damuthrealin@ufl.edu>
2024-04-11 12:52:53 -0400

    additions

--- realin_lab9_decode.py
+++ realin_lab9_decode.py
@@ -1,5 +1,16 @@
 #lab 9 - Realin is Decode
 
+def decode():
+    password_dec = int(input("Insert the password to decode: "))
+    password_dec = list(password_dec)
+    for each_char in range(password_dec):
+        new = []
+        each_char += 3
+        new.append(each_char)
+    new = str(new)
+    return new
+
+
 menu = print("""Menu
 -------------
 1. Encode
@@ -7,3 +18,12 @@ menu = print("""Menu
 3. Quit""")
 
 choice = int(input("Please enter an option: "))
+
+
+while choice == True:
+    if choice == 3:
+        break
+    elif choice == 2:
+        print(decode())
+    elif choice == 1:
+        pass
